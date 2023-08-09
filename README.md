647ab53ef09c4d2d80eaf619dac93c8523ffd776
quintiontang <QuintionTang@gmail.com>
2023-08-09 18:12:49 +0800

    Init:commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # AI-DevSecOps
+
 探索使用 OpenAI、LangChain 和 LlamaIndex（GPT 索引）构建自定义 DevSecOps 知识库
+
+## 架构
+
+整理架构分为两个阶段：
+
+### 数据摄取/索引
+
+![cover](./images/data-indexing.jpg)
+
+### 数据查询
+
+![cover](./images/data-querying.jpg)
+
+## <a id="quick-start" href="#quick-start">🚀 Quick Start</a>
+
+Create a virtual environment to run in.
+
+```
+python3 -m venv venv
+source venv/bin/activate # ubuntu/Mac
+venv\Scripts\activate # windows
+pip3 install --upgrade pip
+pip install -r requirements.txt
+python3 bot.py
+```
+
+## You will see a result like this
+
+![cover](./images/cover.jpg)
---
+++ bot.py
@@ -0,0 +1,69 @@
+from llama_index import StorageContext, ServiceContext, GPTVectorStoreIndex, LLMPredictor, PromptHelper, SimpleDirectoryReader, load_index_from_storage
+from langchain.chat_models import ChatOpenAI
+import gradio as gr
+import sys
+import os
+
+os.environ["OPENAI_API_KEY"] = ''
+
+
+def create_service_context():
+    # 参数配置
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    # 允许用户显式设置某些参数配置
+    prompt_helper = PromptHelper(
+        max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    # LLMPredictor 是 LangChain 的 LLMChain 的包装类，可以轻松集成到 LlamaIndex 中
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(
+        temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+
+    # 构造 service_context
+    service_context = ServiceContext.from_defaults(
+        llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    return service_context
+
+
+def data_ingestion_indexing(directory_path):
+    # 从指定目录路径加载数据
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    # 第一次建立索引时
+    index = GPTVectorStoreIndex.from_documents(
+        documents, service_context=create_service_context()
+    )
+
+    # 持久化索引到磁盘，默认 storage 文件夹
+    index.storage_context.persist()
+
+    return index
+
+
+def data_querying(input_text):
+    # 重建存储上下文
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+
+    # 从存储加载索引
+    index = load_index_from_storage(
+        storage_context, service_context=create_service_context())
+
+    # 用输入文本查询索引
+    response = index.as_query_engine().query(input_text)
+
+    return response.response
+
+
+iface = gr.Interface(fn=data_querying,
+                     inputs=gr.components.Textbox(
+                         lines=7, label="Enter your text"),
+                     outputs="text",
+                     title="DevPoint's Knowledge Base")
+
+# passes in data directory
+index = data_ingestion_indexing("data")
+
+iface.launch(share=False)
